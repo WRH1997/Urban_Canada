75004482f3e9ba4fab7660419a4d4fa47c9f50e4
HARSH NARESHBHAI KATHIRIA <hr527292@dal.ca>
2023-06-10 06:58:26 -0300

    add service booking responsive form

--- frontend/src/components/booking/booking.js
+++ frontend/src/components/booking/booking.js
@@ -145,6 +145,97 @@ export default function Bookings() {
         </>
       )}
     </Disclosure>
+
+    <form className="max-w-sm bg-white py-10 m-auto" action="/MyBookings" method="GET">
+      <div class="mx-8">
+
+
+          <h2 className="text-base font-semibold leading-7 text-xl text-gray-900">Service Booking</h2>
+          <p className="mt-2 text-m leading-6 text-gray-600">
+            Electrician
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 w-max">
+            <div className="sm:col-span-4 ">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md ring-1 ring-gray-300">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value={"John Anderson"}
+                    disabled
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="date" className="block text-sm font-medium leading-6 text-gray-900">
+                Date
+              </label>
+              <div className="mt-2">
+                <input
+                  type="date"
+                  name="date"
+                  id="date"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="time" className="block text-sm font-medium leading-6 text-gray-900">
+                Time
+              </label>
+              <div className="mt-2">
+                <input
+                  type="time"
+                  name="time"
+                  id="time"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Notes
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md ring-1 ring-gray-300">
+                  <textarea
+                    type="textarea"
+                    name="notes"
+                    id="notes"                   
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+      <div className="mt-6 flex items-center justify-center gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Confirm
+        </button>
+      </div>
+      </div>
+    </form>
     </div>
   )
 }
