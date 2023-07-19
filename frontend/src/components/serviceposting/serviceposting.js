4ead66a12c7924d18cee15f392de27acd26109d4
edwin-adams <78889111+edwin-adams@users.noreply.github.com>
2023-07-19 14:31:46 -0300

    temporary pause

--- frontend/src/components/serviceposting/serviceposting.js
+++ frontend/src/components/serviceposting/serviceposting.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "../header/header";
 import Footer from "../footer/footer";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+
 
 export default function ServicePostingPage() {
-    return (
-      <div>
-        <Header currentPage="/serviceposting"/>
-        <h1>Service Posting</h1>
-        
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header currentPage="/serviceposting"/>
+      <h1>Service Posting</h1>
+      
+    </div>
+  );
+}
