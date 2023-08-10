51caef8fd37b920ff2bf4b5f4c869e1fee43787a
Darshil Patel <dr371813@dal.ca>
2023-08-10 13:56:15 -0300

    notification bug fixed

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -222,7 +222,7 @@ export default function Header(props) {
                       {
                     loggedInUser == "service-consumer" || loggedInUser == "service-provider" ?
 
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-72 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-72 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" style={{backgroundColor:"white"}}>
                         {notifications.length > 0 ? notifications.map((item, index) => (
                         <Menu.Item>
                             {/* <div className="d-flex flex-row justify-content-between align-items-center"> */}
@@ -273,7 +273,7 @@ export default function Header(props) {
                     >
                       {loggedInUser == "service-consumer" ||
                       loggedInUser == "service-provider" ? (
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" style={{backgroundColor:"white"}}>
                           <Menu.Item>
                             {({ active }) => (
                               <a
